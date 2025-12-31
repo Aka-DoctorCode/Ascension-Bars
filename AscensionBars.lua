b983b9f91a392513d2d031881452aa08a9653273
Aka-DoctorCode <37858279+Aka-DoctorCode@users.noreply.github.com>
2025-12-31 11:14:25 -0500

    Update AscensionBars.lua
    -- Happy new year everyone!
    -- Please leave feedback to improve the add on

--- AscensionBars.lua
+++ AscensionBars.lua
@@ -1,3 +1,5 @@
+-- Happy new year everyone!
+-- Please leave feedback to improve the add on
 -- ==========================================================
 -- AscensionBars - Version 5.0.0
 -- ==========================================================
